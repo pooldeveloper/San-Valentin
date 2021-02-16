2ca315b39a362782b28ab3c6882e1b1b79a0b8be
unknown <pooldeveloper08@gmail.com>
2021-02-16 12:08:32 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        gray: {
+          100:'#ececec',
+          200:'#8e8e8e'
+        },
+        black: {
+          100:'#222',
+          200:'#636363'
+        },
+        red:'#f51650',
+      }
+    },
+     fontFamily: {
+      pt: ['PT Sans'],
+    },
+    height: {
+      lg: '112px',
+     },
+     spacing: {
+      1: '0.375rem',
+      2: '0.5rem',
+      3: '0.875rem',
+      5: '1.375rem',
+      6: '1.875rem',
+      7: '1.75rem',
+      8: '2rem',
+      9: '2.25rem',
+      10: '2.5rem',
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      20: '5rem',
+      24: '6rem',
+      28: '7rem',
+      32: '8rem',
+      36: '9rem',
+      40: '10rem',
+      44: '11rem',
+      48: '12rem',
+      52: '13rem',
+      56: '14rem',
+      60: '15rem',
+      64: '16rem',
+      72: '18rem',
+      80: '20rem',
+      96: '24rem',
+    }
+  },
+  variants: {
+
+  },
+  plugins: [],
+}
+
